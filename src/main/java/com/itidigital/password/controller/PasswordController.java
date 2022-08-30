8ccee32f86f3441e16a4b844cb3e89198afb2720
sarah-lima <sarah.lima@bandtec.com.br>
2022-08-30 01:59:31 -0300

    ajuste cross origin

--- src/main/java/com/itidigital/password/controller/PasswordController.java
+++ src/main/java/com/itidigital/password/controller/PasswordController.java
@@ -5,11 +5,13 @@ import com.itidigital.password.service.PasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@CrossOrigin
 @RequestMapping("/")
 public class PasswordController {
     @Autowired
